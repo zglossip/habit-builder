e889799675804d6c427495e68ab26e7addfe505b
Zach Glossip <zglossip@gmail.com>
2024-01-19 13:14:17 -0600

    Add storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
-  ],
+  'extends': ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/typescript/recommended', 'plugin:storybook/recommended'],
   parserOptions: {
     ecmaVersion: 2020
   },
