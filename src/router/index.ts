2f5b1514c66717377245e2ef390b206824a0443a
Zach Glossip <zglossip@gmail.com>
2024-01-21 14:43:00 -0600

    Fix router

--- src/router/index.ts
+++ src/router/index.ts
@@ -25,7 +25,7 @@ const routes: Array<RouteRecordRaw> = [
     }),
   },
   {
-    path: "/counter",
+    path: "/counter/:counterId",
     name: "ViewCounter",
     component: () => import("../views/CounterView/CounterView.vue"),
     props: ({ params }) => ({
